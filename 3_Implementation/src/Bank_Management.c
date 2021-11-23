6e1832a068d43fbfcee1691ff0fab99ebd9a2423
npbhavaniprasad <72255681+npbhavaniprasad@users.noreply.github.com>
2021-11-23 22:52:13 +0530

    Update Bank_Management.c

--- 3_Implementation/src/Bank_Management.c
+++ 3_Implementation/src/Bank_Management.c
@@ -1,3 +1,5 @@
+// CLIENT SECTION (MAIN) //
+
 #include <stdio.h>
 #include <string.h>
 
@@ -6,64 +8,82 @@
 
 int main()
 {
-    char menu_choice[2], temp_str[16], temp_str2[16], temp_str3[16];
+    char id[16], pass[8], menu_choice[2], temp_str[16];
 
     while(1)
     {
-        control_menu(70);
-        scanf("%s", menu_choice);
+        // Login Screen //
+        screen(40, "ID", 1, 16);
+        scanf("%s", id);
 
-        if (strstr(menu_choice, "q")) { break; }
-        else if (strstr(menu_choice, "1"))
+        if (strstr(id, "create"))
         {
             screen(40, "Please enter your password", 13, 16);
-            scanf("%s", temp_str);
-
-            if (strstr(temp_str, "q")) {  }
-            else { create_account(temp_str); }
+            scanf("%s", pass);
+            create_account(pass);
+            return 0;
         }
-        else if (strstr(menu_choice, "2"))
+
+        screen(40, "PASSWORD", 4, 18);
+        scanf("%s", pass);
+        // ============ //
+
+        // Ban Check //
+        write_log(id, "logged in the account\n");
+
+        if ( check_ban(id) == -1)
         {
-            screen(40, "Enter account id", 8, 16);
-            scanf("%s", temp_str);
+            screen(40, "!BANNED ACCOUNT!", 8, 0);
+            return -1; 
+        }
+        else { write_log(id, "checked the account was banned or not\n"); }
+        // ========= //
 
-            screen(40, "Enter password id", 8, 16);
-            scanf("%s", temp_str3);
+        // Menu Screen //
+        while(1)
+        {
+            menu(id, pass, 70);
+            scanf("%s", menu_choice);
 
-            screen(40, "As admin or user?", 8, 18);
-            scanf("%s", temp_str2);
+            if (strstr(menu_choice, "q")) { break; }
+            else if (strstr(menu_choice, "1"))
+            {
+                screen(40, "How much to deposit?", 10, 16);
+                scanf("%s", temp_str);
 
-            if (strstr(temp_str, "q")) {  }
-            else
-            { 
-                if (strstr(temp_str2, "user")) { delete_account(temp_str, temp_str3,1); }
-                else if (strstr(temp_str2, "admin")) { delete_account(temp_str, temp_str3, 2); }
+                if (strstr(temp_str, "q")) {  }
+                else { deposit(id, pass, temp_str); }
+            }
+            else if (strstr(menu_choice, "2"))
+            {
+                screen(40, "How much to withdraw?", 10, 16);
+                scanf("%s", temp_str);
+
+                if (strstr(temp_str, "q")) {  }
+                else { withdraw(id, pass, temp_str); }
+            }
+            else if (strstr(menu_choice, "3"))
+            {
+                screen(40, "What is your new password?", 13, 18);
+                scanf("%s", temp_str);
+
+                if (strstr(temp_str, "q")) {  }
+                else { change_pass(id, pass, temp_str); }
+                break;
+            }
+            else if (strstr(menu_choice, "4"))
+            {
+                screen(70, "Do you really want to permanently delete your account?", 27, 2);
+                scanf("%s", temp_str);
+
+                if (strstr(temp_str, "q")) {  }
+                else { delete_account(id, pass, 1); }
+                break;
             }
         }
-        else if (strstr(menu_choice, "3"))
-        {
-            screen(40, "ID", 1, 16);
-            scanf("%s", temp_str);
-
-            screen(40, "OLD PASSWORD", 6, 18);
-            scanf("%s", temp_str2);
-
-            screen(40, "NEW PASSWORD", 6, 18);
-            scanf("%s", temp_str3);
-            
-            if (strstr(temp_str, "q") || strstr(temp_str2, "q") || strstr(temp_str3, "q")) {  }
-            else { change_pass(temp_str, temp_str2, temp_str3); }
-        }
-        else if (strstr(menu_choice, "4"))
-        {
-            screen(40, "Enter account id", 13, 16);
-            scanf("%s", temp_str);
-            
-            if (strstr(temp_str, "q")) {  }
-            else { ban_account(temp_str); }
-        }
-    }
     clrscr();
+    // =========== //
+    }
 
     return 0;
 }
